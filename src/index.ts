ea43d1328bd2eaa704baf306d8c15918b78d6a48
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-11-13 23:33:07 -0300

    calling API with timestamp only

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import request from 'request'
 
+const timestamp = Date.now()
+request(`http://gateway.marvel.com/v1/public/comics?ts=${timestamp}`,(error, response, body)=>{
+
+})
 const app = express()
 
 app.get('/',(_,res)=>{
-    res.send('Marvel Stories go here')
+    request(`http://gateway.marvel.com/v1/public/comics?ts=${timestamp}`,(error, response, body)=>{
+    console.log(error)
+    console.log(response)
+    res.send(body)
+
+})
+    // res.send('Marvel Stories go here')
 })
 
 app.listen(8000,()=>{
