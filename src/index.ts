18ef711f2af2c5c1182be853d0e0e3ddc0d52a23
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-11-14 01:13:32 -0300

    synchronous call to stories after heroes ids

--- src/index.ts
+++ src/index.ts
@@ -11,30 +11,58 @@ const md5 = new Md5()
 const hash = md5.appendStr(timestamp).appendStr(privateKey).appendStr(publicKey).end()
 const heroes = ['Iron Man', 'Thor', 'Doctor Strange']
 let characters:string[] = []
-heroes.forEach(hero=>{
-    request(
-        `http://gateway.marvel.com/v1/public/characters?name=${hero}&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`,
-        (error, _, bodyString)=>{
-            if(error){
-                console.log(error)
-            }
-            const body = JSON.parse(bodyString)
-            characters.push(body.data.results[0].id)
-    })
 
-})
+const getHeroId = (hero:string):Promise<string> =>{
+    return new Promise<string>((resolve,reject)=>{
+        request(
+            `http://gateway.marvel.com/v1/public/characters?name=${hero}&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`,
+            (error, _, bodyString)=>{
+                if(error){
+                    console.log(error)
+                    reject(error)
+                }
+                const body = JSON.parse(bodyString)
+                resolve(body.data.results[0].id)
+        })
+    })
+}
+
+
+// heroes.forEach(hero=>{
+//     request(
+//         `http://gateway.marvel.com/v1/public/characters?name=${hero}&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`,
+//         (error, _, bodyString)=>{
+//             if(error){
+//                 console.log(error)
+//             }
+//             const body = JSON.parse(bodyString)
+//             characters.push(body.data.results[0].id)
+//     })
+
+// })
 
 const app = express()
 
 app.get('/',(_,res)=>{
-    request(
-        `http://gateway.marvel.com/v1/public/stories?apikey=${publicKey}&ts=${timestamp}&hash=${hash}&characters=${characters.join(',')}&limit=${storiesQuantity}`,
-        (error, _, body)=>{
-            res.send(body)
-            if(error){
-                console.log(error)
-            }
+    Promise.all(heroes.map(getHeroId))
+    .then(charactersIds=>{
+        request(
+            `http://gateway.marvel.com/v1/public/stories?apikey=${publicKey}&ts=${timestamp}&hash=${hash}&characters=${charactersIds.join(',')}&limit=${storiesQuantity}`,
+            (error, _, body)=>{
+                res.send(body)
+                if(error){
+                    console.log(error)
+                }
+        })
     })
+    // request(
+    //     `http://gateway.marvel.com/v1/public/stories?apikey=${publicKey}&ts=${timestamp}&hash=${hash}&characters=${characters.join(',')}&limit=${storiesQuantity}`,
+    //     (error, _, body)=>{
+    //         res.send(body)
+    //         if(error){
+    //             console.log(error)
+    //         }
+    // })
 })
 
 app.listen(8000,()=>{
