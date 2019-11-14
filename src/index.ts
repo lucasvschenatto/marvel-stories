96b63286bcde511f6e2af2f3838d7f50fd6a83c2
Lucas Schenatto <lucasvschenatto@users.noreply.github.com>
2019-11-14 15:21:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {Md5} from 'ts-md5'
 
 const timestamp = Date.now().toString()
 const publicKey = '6342bb2f938ce53effd19d116f5c23f0'
-const privateKey = '108ae3b64060b90637c1a08f389d9b01e64183b7'
+const privateKey = 'PRIVATE-KEY'
 const storiesQuantity = 5
 const md5 = new Md5()
 const hash = md5.appendStr(timestamp).appendStr(privateKey).appendStr(publicKey).end()
