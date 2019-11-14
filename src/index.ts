c2c3ece883882dbc4d82f03194b4e6971471989d
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-11-14 00:34:04 -0300

    reading unsynchronized heores and stories

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,23 @@ import {Md5} from 'ts-md5'
 const timestamp = Date.now().toString()
 const publicKey = '6342bb2f938ce53effd19d116f5c23f0'
 const privateKey = '108ae3b64060b90637c1a08f389d9b01e64183b7'
-const characters = [1009368, 1009664, 1009282]
 const storiesQuantity = 5
 const md5 = new Md5()
 const hash = md5.appendStr(timestamp).appendStr(privateKey).appendStr(publicKey).end()
+const heroes = ['Iron Man', 'Thor', 'Doctor Strange']
+let characters:string[] = []
+heroes.forEach(hero=>{
+    request(
+        `http://gateway.marvel.com/v1/public/characters?name=${hero}&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`,
+        (error, _, bodyString)=>{
+            if(error){
+                console.log(error)
+            }
+            const body = JSON.parse(bodyString)
+            characters.push(body.data.results[0].id)
+    })
+
+})
 
 const app = express()
 
