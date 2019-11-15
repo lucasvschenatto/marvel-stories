819e13ddbf35e481c7b64e12203abdf3225982b4
Lucas Schenatto <lucasvschenatto@hotmail.com>
2019-11-15 11:46:06 -0300

    moving config data to other files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express'
 import request from 'request'
+import APIKeys from './APIKeys'
+import UserPreferences from './UserPreferences'
 import {Md5} from 'ts-md5'
 
 
 const timestamp = Date.now().toString()
-const publicKey = '6342bb2f938ce53effd19d116f5c23f0'
-const privateKey = '108ae3b64060b90637c1a08f389d9b01e64183b7'
-const storiesQuantity = 5
+const {publicKey, privateKey} = APIKeys
+const {storiesQuantity, heroes} = UserPreferences
 const md5 = new Md5()
 const hash = md5.appendStr(timestamp).appendStr(privateKey).appendStr(publicKey).end()
-const heroes = ['Iron Man', 'Thor', 'Doctor Strange']
 let characters:string[] = []
 
 const getHeroId = (hero:string):Promise<string> =>{
